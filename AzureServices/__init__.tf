10ba2503d6646f3a406491f95d1e98241478c2eb
tminhein <tunminster@gmail.com>
2019-07-08 11:37:02 +0100

    removed the keys

--- AzureServices/__init__.tf
+++ AzureServices/__init__.tf
@@ -4,8 +4,8 @@ terraform {
     storage_account_name = "csb2359b88d3130x4c42xa9f"
     container_name       = "test-tfstate"
     key                  = "dev.terraform.test-tfstate"
-    access_key="ZyCTHCmdMEHSpDxUh5fOTIys7RiW0YlqtXEREOoO941Dc3x/ZVQGViNzPiFM1EEmaFqLDR7OM9xfL+FEMa1SqA=="
-    subscription_id  = "2359b88d-3130-4c42-a9fb-2455697edb51"
+    access_key="Ma1SqA=="
+    subscription_id  = "xxxxxxxxxxxxxxxx-2455697edb51"
     tenant_id        = "11121435-b640-4aca-944d-53645b9d42da"
   }
 }
